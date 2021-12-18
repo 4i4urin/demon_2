c5d247f5fb265a55af32eef6d784d5425bf5a50c
4i4urin <56293411+4i4urin@users.noreply.github.com>
2021-12-18 13:56:45 +0300

    Update Demon2_3.m

--- Demon2_3.m
+++ Demon2_3.m
@@ -4,14 +4,14 @@ syms x;
 syms w;
 
 % Шиша
-Nzad = 19200;
-sigma = 0.07;
-Emax = 1.2;
-w_mid = 36;
-Kg = 62500;
-Ws = 91428.5714286 / (s^3 + (810*s^2)/7 + (20000)*s/7);
-W_ky = (1.75e-05 * s^3 + 0.002106 * s^2 + 0.081 * s + 1) / (1.161e-05 * s^3 + 0.02897 * s^2 + 18.04 * s + 1);
-W = (0.0005599 * s^3 + 0.06739 * s^2 + 2.592 * s + 32) / (4.063e-09 * s^5 + 1.061e-05 * s^4 + 0.007498 * s^3 + 0.7598 * s^2 + 18.08 * s + 1);
+% Nzad = 19200;
+% sigma = 0.07;
+% Emax = 1.2;
+% w_mid = 36;
+% Kg = 62500;
+% Ws = 91428.5714286 / (s^3 + (810*s^2)/7 + (20000)*s/7);
+% W_ky = (1.75e-05 * s^3 + 0.002106 * s^2 + 0.081 * s + 1) / (1.161e-05 * s^3 + 0.02897 * s^2 + 18.04 * s + 1);
+% W = (0.0005599 * s^3 + 0.06739 * s^2 + 2.592 * s + 32) / (4.063e-09 * s^5 + 1.061e-05 * s^4 + 0.007498 * s^3 + 0.7598 * s^2 + 18.08 * s + 1);
 
 % Журов
 % sigma = 7;
@@ -22,9 +22,20 @@ W = (0.0005599 * s^3 + 0.06739 * s^2 + 2.592 * s + 32) / (4.063e-09 * s^5 + 1.06
 % Nzad = 8000;
 % Ws = (56.5*8750) / ((0.026 * s + 1) * (0.0055 * s + 1) * s);
 
+% Ws = input("Введите передаточную функцию разомкнутой системы: ");
+% [num, den] = numden(Ws);
+% Ws_tf = tf(sym2poly(num), sym2poly(den));
 
+sigma = input("Введите сигму: ");
+Tmax = input("Введите Tпмакс: ");
+Emax = input("Введите Eмакс: ");
+
+Ws = input("Введите передаточную функцию разомкнутой системы: ");
+[num, den] = numden(Ws);
+Ws_tf = tf(sym2poly(num), sym2poly(den));
 
 disp("По теореме Котельникова определим период квантования");
+w_mid = b / Tmax;
 T = pi / w_mid;
 disp(T);
 disp("Возьмём период квантование не более данного");
@@ -40,168 +51,78 @@ disp("N = ");
 disp(N);
 
 disp("Передаточная функция разомкнутой системы с требуемым усилением");
-disp(vpa(Ws, 5));
+disp(Ws_tf);
 
+Wz = c2d(Ws_tf, T);
+display(zpk(Wz));
 
-disp("Проведём z-преобразование:");
-[numArr, denArr] = before_tf(Wnch, 2);
-Wnch_tf = tf(numArr, denArr);
-display(Wnch_tf);
-
-z_trans = c2d(Wnch_tf, T, 'impulse') * 1/T^2;
-display(zpk(z_trans));
-
-
-[n, d] = tfdata(z_trans);
-
-n = round(cell2mat(n), 9); d = cell2mat(d);
-n = circshift(n, 1);
-d = divide_by_z_1(d);
-Wnch_ztrans = tf(n, d);
-display(zpk(Wnch_ztrans));
-
-[num, den] = tfdata(Wnch_ztrans);
+[num, den] = tfdata(Wz);
+Wz = poly2sym(cell2mat(num), z) / poly2sym(cell2mat(den), z);
+Wz = subs(Wz, z, (1 + w) / (1 - w));
+[num, den] = numden(Wz);
+Wz = tf(sym2poly(num), sym2poly(den));
+display(zpk(Wz));
+[num, den] = tfdata(Wz);
 num = cell2mat(num); den = cell2mat(den);
+k = num(max(size(num))) / den(max(size(den)) - 1);
+num = poly2sym(num); den = poly2sym(den);
 
-replace = vpa(poly2sym(num, w) / poly2sym(den, w), 5);
-replace = subs(replace, w, (1 + w) / (1 - w));
-[rep_num, rep_den] = numden(replace);
-replace_tf = zpk(tf(sym2poly(rep_num), sym2poly(rep_den)));
-display(replace_tf);
-
-[n, d] = tfdata(replace_tf);
-n = round(cell2mat(n), 5); d = round(cell2mat(d), 5);
-show_replace = vpa(poly2sym(n, w)/poly2sym(d, w), 5);
-display(show_replace);
-
-[num, den] = numden(show_replace);
-num = vpa(factor(num, 'FactorMode', 'real'), 5);
-den = vpa(factor(den, 'FactorMode', 'real'), 5);
-disp(num); disp(den);
-
-% костыль
-% построение неизменяемого графика
-vc_arr_den = zeros(2, 0);
-vc_arr_num = zeros(3, 0);
-for i = 1:max(size(vc_arr_den))
-    vc_arr_den(i) = vpa(min(abs(coeffs(den(2 + i)))), 5);
+num = factor(num, 'FactorMode', 'real');
+den = factor(den, 'FactorMode', 'real');
+% disp(num); disp(den);
+vc_arr_num = zeros(max(size(num)) - 1, 0);
+vc_arr_den = zeros(max(size(den)) - 1, 0);
+for i = 1:(max(size(num)) - 1)
+    b = abs(coeffs(num(i + 1)));
+    if b(1) < 0.0001 || max(size(b)) < 2 % в случае если скобка x а не x + a1
+        b = 0;
+    end
+    vc_arr_num(i) = b(1);
 end
-for i = 1:max(size(vc_arr_num))
-    vc_arr_num(i) = vpa(max(abs(coeffs(num(1 + i)))), 5);
+for i = 1:(max(size(den)) - 1)
+    b = abs(coeffs(den(i + 1)));
+    if b(1) < 0.0001 || max(size(b)) < 2 % в случае если скобка x а не x + a1
+        b = 0;
+    end
+    vc_arr_den(i) = b(1);
 end
-
-vc_arr_den = sort(vc_arr_den);
-vc_arr_num = sort(vc_arr_num);
-%костыль
-vc1 = vc_arr_den(1);
-vc2 = vc_arr_den(2);
-vc3 = vc_arr_num(1);
-vc4 = vc_arr_num(2);
-vc5 = vc_arr_num(3);
-
-disp("vc1 = " );
-disp(vc1);
-disp("vc2 = " );
-disp(vc2);
-disp("vc3 = " );
-disp(vc3);
-disp("vc4 = " );
-disp(vc4);
-disp("vc5 = " );
-disp(vc5);
-k = abs(num(1) * vc4 * vc5 / (den(1) * vc1 * vc2));
+vc = [vc_arr_den, vc_arr_num];
+vc = sort(vc);
+vc(vc==0) = []; % удвление 0 из массива
+disp("Vc от vc1 до vc5");
+disp(vc);
 disp(k);
+
+k_high = 20 * log10(k);
 disp("20 * log(k) = ");
-k = 20* log10(k);
-disp(k);
+disp(k_high);
+disp("По этим данным надо график посторить");
 
-% наверно надо чтобы пользователь вводил нижнюю и верхнюю границу гррафика
-x_begin = 0.000001;
-x1 = vc1;
-x2 = vc2;
-x3 = vc3; % = 1;
-x4 = vc4;
-x5 = vc5;
-x_end = 100;
+disp("Праметры желаемой ЛАЧХ");
+B = containers.Map('KeyType', 'double', 'ValueType', 'double');
+B(5) = 6.5; B(10) = 6.7; B(20) = 6.9; B(25) = 8.8; B(30) = 11.3; 
+B(35) = 14.1; B(40) = 16.9;
 
-y3 = k;
-y2 = y3 + 60 * abs(log10(x3 / x2));
-y1 = y2 + 40 * abs(log10(x2 / x1));
-y_begin = y1 + 20 * abs(log10(x1 / x_begin));
+k = cell2mat(keys(B));
+b = 0;
+for i = 1:length(B)
+    key = k(i);
+    if (key > sigma)
+        break;
+    end
+    b = B(key);
+end
 
-y4 = y3 - 40 * abs(log10(x4 / x3));
-y5 = y4 - 20 * abs(log10(x5 / x4));
-y_end = y5;
 
-semilogx([x_begin, x1, x2, x3, x4, x5, x_end], [y_begin, y1, y2, y3, y4, y5, y_end]);
-grid on;
-hold on;
 
-% построение желаемого графика
-disp("Рассчитаем параметры желаемой ЛАЧХ");
+disp("wсрж = ");
 disp(w_mid);
 v_mid = tan(w_mid * T / 2);
-disp(v_mid);
 v_low = 0.16 * v_mid;
 v_high = 6.5 * v_mid;
-
-y_mid = 0;
-y_low = y_mid + 20 * abs(log10(v_low) - log10(v_mid));
-y_begin = y_low + 40 * abs(log10(x_begin) - log10(v_low));
-y_high = y_mid - 20 * abs(log10(v_mid) - log10(v_high));
-y_end = y_high;
-
-semilogx([x_begin v_low, v_mid, v_high, x_end], [y_begin, y_low, y_mid, y_high, y_end], 'g');
-hold on;
-
-disp("частоты перегиба");
-disp("w1 = ");
+disp("vсрж = ");
+disp(v_mid);
+disp("vн = ");
 disp(v_low);
-disp("w2 = ");
-disp(vc1);
-disp("w3 = ");
-disp(vc2);
-disp("w4 = ");
+disp("vв = ");
 disp(v_high);
-disp("w5 = ");
-disp(vc5);
-disp("w6 = ");
-disp(vc3);
-disp("w7 = ");
-disp(vc4);
-disp("w8 = ");
-disp("Возможно образование дополнительных точек перегиба смотри график");
-
-
-% флаг указывает на наличие константы в знаменателе 
-% если флаг 1 то знаменатель a1 * s^n + a2 * s^(n-1) + ... + an * s^flag
-% если флаг 0 то знаменатель a1 * s^n + a2 * s^(n-1) + ... + an * s + const
-function [numArr, denArr] = before_tf(W, flag)
-    [n, d] = numden(W);
-    num = coeffs(n);
-    den = coeffs(d);
-    numArr = zeros(1, max(size(num)));
-    if (flag > 0)
-        denArr = zeros(1, max(size(den)) + flag);
-    else
-        denArr = zeros(1, max(size(den)));
-    end
-    koef = den(1);
-    for i = 1:max(size(num))
-        numArr(i) = num(max(size(num)) - i + 1) / koef;
-    end
-    for i = 1:(max(size(den)))
-        denArr(i) = den(max(size(den)) - i + 1) / koef;
-    end
-    % подставляем в последний член 0 чтобы избавится от константы в
-    % знаменателе
-    %denArr(max(size(den)) + 1) = 0;
-end
-
-function den_new = divide_by_z_1(den_old)
-    den_new = zeros(max(size(den_old)) - 1, 0);
-    den_new(1) = den_old(1);
-    for i = 2:max(size(den_old) - 1)
-        den_new(i) = den_new(i - 1) + den_old(i);       
-    end
-end
