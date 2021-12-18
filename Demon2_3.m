869ff49867e8c667dec88ea31e49d166eb46ac25
4i4urin <56293411+4i4urin@users.noreply.github.com>
2021-12-18 15:11:20 +0300

    Update Demon2_3.m

--- Demon2_3.m
+++ Demon2_3.m
@@ -3,6 +3,20 @@ syms s;
 syms x;
 syms w;
 
+B = containers.Map('KeyType', 'double', 'ValueType', 'double');
+B(5) = 6.5; B(10) = 6.7; B(20) = 6.9; B(25) = 8.8; B(30) = 11.3; 
+B(35) = 14.1; B(40) = 16.9;
+
+k = cell2mat(keys(B));
+b = 0;
+for i = 1:length(B)
+    key = k(i);
+    if (key > sigma)
+        break;
+    end
+    b = B(key);
+end
+
 % Шиша
 % Nzad = 19200;
 % sigma = 0.07;
@@ -36,7 +50,9 @@ Ws_tf = tf(sym2poly(num), sym2poly(den));
 
 disp("По теореме Котельникова определим период квантования");
 w_mid = b / Tmax;
-T = pi / w_mid;
+w_high = 6.5 * w_mid;
+% disp(w_high);
+T = pi / w_high;
 disp(T);
 disp("Возьмём период квантование не более данного");
 
@@ -51,8 +67,7 @@ disp("N = ");
 disp(N);
 
 disp("Передаточная функция разомкнутой системы с требуемым усилением");
-disp(Ws_tf);
-
+display(Ws_tf);
 Wz = c2d(Ws_tf, T);
 display(zpk(Wz));
 
@@ -92,11 +107,37 @@ vc(vc==0) = []; % удвление 0 из массива
 disp("Vc от vc1 до vc5");
 disp(vc);
 disp(k);
-
 k_high = 20 * log10(k);
 disp("20 * log(k) = ");
 disp(k_high);
 disp("По этим данным надо график посторить");
+
+vc_begin = 1e-6;
+% vc1 = 0.1853;
+% vc2 = 0.3969;
+% vc3 = 1;
+% vc4 = 1.4849;
+% vc5 = 2.1154;
+vc_end = 1e3;
+
+% k_y = 54.4856;
+
+vc_begin_y = k_high + 20 * abs(log10(1 / vc_begin));
+vc1_y = vc_begin_y - 20 * abs(log10(vc_begin / vc(1)));
+vc2_y = vc1_y - 40 * abs(log10(vc(1) / vc(2)));
+vc3_y = vc2_y - 60 * abs(log10(vc(2) / vc(3)));
+vc4_y = vc3_y - 40 * abs(log10(vc(3) / vc(4)));
+vc5_y = vc4_y - 20 * abs(log10(vc(4) / vc(5)));
+vc_end_y = vc5_y;
+% vc1
+
+x = [vc_begin, vc(1), vc(2), vc(3), vc(4), vc(5), vc_end];
+y = [vc_begin_y, vc1_y, vc2_y, vc3_y, vc4_y, vc5_y, vc_end_y];
+
+semilogx(x, y);
+grid on;
+hold on;
+
 
 disp("Праметры желаемой ЛАЧХ");
 B = containers.Map('KeyType', 'double', 'ValueType', 'double');
@@ -126,3 +167,15 @@ disp("vн = ");
 disp(v_low);
 disp("vв = ");
 disp(v_high);
+
+
+v_mid_y = 0;
+v_low_y = v_mid_y + 20 * abs(log10(v_low / v_mid));
+v_high_y = v_mid_y - 20 * abs(log10(v_mid / v_high));
+vc_begin_y = v_low_y + 40 * abs(log10(v_low / vc_begin));
+vc_end_y = v_high_y;
+
+x_l = [vc_begin, v_low, v_mid, v_high, vc_end];
+y_l = [vc_begin_y, v_low_y, v_mid_y, v_high_y, vc_end_y];
+semilogx(x_l, y_l);
+grid on;
