23ba4fea0d6023f65a3d85392ca19e426ebcacfc
4i4urin <56293411+4i4urin@users.noreply.github.com>
2021-12-18 15:11:37 +0300

    Update Demon2_4.m

--- Demon2_4.m
+++ Demon2_4.m
@@ -9,28 +9,19 @@ syms x;
 
 % Журов
 
-Wky_s = (s * 66.667 + 1) * (s * 47.62 + 1) * (s * 10 + 1) * (s * 2 + 1) / +...
-    ((s * 3703703.703 + 1) * (s + 1) * (s * 0.597 + 1) * (s * 0.5571 + 1));
-Ws = 562.5 * 8750 / ((0.026 * s + 1) * (0.0055 * s + 1) * s);
-T = 0.001309;
-% v_mid = 0.083;
-% Wky = (0.033 * s + 1)*(0.026 * s + 1)*(0.0055 * s + 1) /+...
-%     ((21.8 * s + 1)*(0.00042 * s + 1)*(0.00042 * s + 1));
+w = zeros(1, 8);
+disp(['Введите точки абсциссы точек перегиба полученного графика слева' ...
+    ' направо (их должно быть 8):']);
+for i = 1:8
+    w(i) = 1 / input("Точка " + i + ": ");
+end
 
-%Чел
-% Wky_s = (1 / 0.023 * s + 1) * (1 / 0.023 * s + 1) * (1 / 0.045 * s + 1) / +...
-%     ((1 / 0.0000007 * s + 1) * (s + 1) * (1 / 1.698 * s + 1) * (1 / 1.767 * s + 1));
-% Ws = 528.125 * 9375 / (s * (0.011 * s + 1) * (0.022 * s + 1));
-% T = 0.002618;
+Wky_s = (w(2) * s + 1) * (w(3) * s + 1) * (w(4) * s  + 1) * (w(5) * s + 1) / +...
+    ((w(1) * s + 1) * (w(6) * s + 1) * (w(7) * s + 1) * (w(8) * s + 1));
 
+T = input("Введиете дискретность графика T = ");
 
-
-
-% Демон
-% Wky_s = (1 / 0.033 * s) * (2.16 * s + 1) / ((1 / 0.005 * s + 1) * (s + 1));
-% Ws = 40 / (s * (0.1 * s + 1));
-% T = 0.1;
-
+Ws = input("Введите передаточную функцию разомкнутой системы: ");
 [n, d] = numden(Ws);
 Ws_tf = tf(sym2poly(n), sym2poly(d));
 display(Ws_tf);
@@ -39,6 +30,8 @@ Wky = simplify(subs(Wky_s, s, (z - 1) / (z + 1)));
 display(Wky);
 [num, den] = numden(Wky);
 Wky_z = tf(sym2poly(num), sym2poly(den));
+disp("После подстановки w = (1 - z) / (z + 1)");
+disp("В отчёте s заменить на Z");
 display(zpk(Wky_z));
 
 
@@ -48,25 +41,11 @@ disp("Реализацив в виде Z-формы");
 Z_ky = filt(sym2poly(num), sym2poly(den), T);
 display(Z_ky);
 
-% пункт 14
-% Даааа ебать того рот
-
-Wky_fromz = d2c(Z_ky, 'tustin');
-display(Wky_fromz);
-W = Ws_tf * Wky_fromz;
-display(W);
-W = W / (1 + W);
-Wd = c2d(W, T);
-step(Wd);
-
-
-
-% разностное уровнение рот ебал 
-% может правильно а может и нет
-% по всей видимотси не тем нум ден
+disp("По Wку(z) получаем разностное уравнение");
 num = sym2poly(num);
 den = sym2poly(den);
-% display(num); display(den);
+num = num / den(1);
+den = den / den(1);
 s = "";
 for i = 1:max(size(num))
     s = s + num2str(num(i)) + " * e[(k - " + num2str(i - 1) + ")T] ";
@@ -82,3 +61,18 @@ for i = 1:max(size(den))
     end
 end
 disp(s);
+
+% пункт 14
+% Даааа ебать того рот
+
+Wky_fromz = d2c(Z_ky, 'tustin');
+display(Wky_fromz);
+W = Ws_tf * Wky_fromz;
+display(W);
+W = W / (1 + W);
+Wd = c2d(W, T);
+step(Wd);
+grid on;
+hold on;
+S = stepinfo(Wd);
+disp(S);
